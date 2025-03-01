0ecf889bd41ba93ff8e12548a26bbafe2dfce642
Michael Maggs <seattleslew@runawayserver.com>
2025-03-01 15:56:10 -0800

    terraform for k8s cluster

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "pol-ayt"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "polat"
+}
+
+variable "hosted-zone" {
+  default = "clarusway.us"
+}
