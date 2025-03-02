9f4ea3516eb794fb79e64c1fb5a99ec546761fd9
Michael Maggs <seattleslew@runawayserver.com>
2025-03-01 16:05:42 -0800

    adding tf files back in

---
+++ tf-files/variables.tf
@@ -0,0 +1,13 @@
+variable "git-name" {
+  default = "maggs"
+}
+
+# add git-token variable here
+
+variable "key-name" {
+  default = "core"
+}
+
+variable "hosted-zone" {
+  default = "infinitelyloopy.com"
+}
